f93082076dddd75c8ef54bad94dcf56341ef33db
kevin <kevinlekiller@gmail.com>
2016-08-14 09:50:50 -0400

    More command line opts, better print handling.

--- mkvrg.py
+++ mkvrg.py
@@ -9,36 +9,52 @@ from argparse import ArgumentParser
 
 def main(argv):
     mkvrg = Mkvrg("mkvrg")
-    return mkvrg.start(parse_args(mkvrg))
+    mkvrg.start(parse_args(mkvrg))
+    return 0
 
 
 def parse_args(mkvrg):
+    """Parse command line arguments."""
     parser = ArgumentParser()
-    parser.add_argument("-m", "--minsize", type=int, help="Minimum size of matroska file in MB")
+    parser.add_argument("-m", "--minsize", type=int, help="Minimum size of matroska file in MB", default=0)
     parser.add_argument("-c", "--verify", help="Verify if matroska file has replaygain tags before and after analyzing.", action="store_true")
     parser.add_argument("-f", "--force", help="Force scanning files for replaygain, even if they already have replaygain tags.", action="store_true")
+    parser.add_argument("-e", "--exit", help="Stop and exit if any problems are encountered during file processing.", action="store_true")
+    parser.add_argument("-v", "--verbosity", type=int, help="Level of verbosity. (0 = normal, -1 = silent, 1 = debug).", default=0, choices=[-1,0,1])
     parser.add_argument("paths", help="Path(s) to folder(s) or file(s) to scan matroska files for replaygain info.", nargs="*")
     args = parser.parse_args()
-    if args.force:
-        mkvrg.force = True
-    if args.verify:
-        mkvrg.verify = True
-    if args.minsize > 0:
-        mkvrg.minsize = args.minsize
+    mkvrg.exit = args.exit
+    mkvrg.force = args.force
+    mkvrg.minsize = args.minsize
+    mkvrg.verify = args.verify
+    mkvrg.verbosity = args.verbosity
     if not args.paths:
-        print("No path(s) given, processing current working directory recursively.")
+        mkvrg.print_message("No path(s) given, processing current working directory recursively.", mkvrg.MINFO)
         return ["."]
     return args.paths
 
 
 class Mkvrg:
+    VERBOSITY_SILENT = -1
+    VERBOSITY_NORMAL = 0
+    VERBOSITY_DEBUG = 1
+
+    MDEBUG = 4
+    MERROR = 3
+    MWARNING = 2
+    MNOTICE = 1
+    MINFO = 0
+
     def __init__(self, name):
         self.name = name
+        self.exit = False
         self.force = False
         self.minsize = 0
+        self.verbosity = self.VERBOSITY_NORMAL
         self.verify = False
 
     def start(self, args):
+        """Sort through paths to see if they are directories or normal files."""
         path = os.path
         for arg in args:
             if path.isdir(arg):
@@ -46,9 +62,10 @@ class Mkvrg:
             elif path.isfile(arg):
                 self.process_file(arg)
             else:
-                print("This does not look like a valid path: " + arg)
+                self.print_message("This does not look like a valid path: " + arg, self.MNOTICE)
 
     def process_dir(self, directory):
+        """Find mkv/mkva/mk3d files in directories"""
         for rootdir, dirnames, filenames in os.walk(directory):
             files = fnmatch.filter(filenames, '*.[mM][kK][aAvV]')
             files.extend(fnmatch.filter(filenames, '*.[mM][kK]3[dD]'))
@@ -56,16 +73,47 @@ class Mkvrg:
                 self.process_file(os.path.join(rootdir, filename))
 
     def process_file(self, path):
-        print(path)
+        """Process a matroska file, analyzing it with bs1770gain and applying tags."""
+        self.print_message("Processing file: " + path)
         if not self.test_matroska(path):
+            self.print_message("File is not matroska.", self.MWARNING)
+            if (self.exit):
+                exit(1)
             return
+
+    def check_tags(self, path):
+        """Check if matroska file has replaygain tags."""
+        print()
+
+    def do_exit(self, code=1):
+        """Exit if --exit option is enabled."""
+        if self.exit:
+            self.print_message("The --exit option is enabled, exiting.", self.MINFO)
+            exit(code)
+
+    def print_message(self, message, mtype=0):
+        """Print message with colors, appends type of message."""
+        if self.verbosity == self.VERBOSITY_SILENT:
+            return
+        if mtype == self.MDEBUG and self.verbosity != self.VERBOSITY_DEBUG:
+            return
+        if mtype == self.MINFO:
+            print("INFO: " + message)
+        elif mtype == self.MNOTICE:
+            print("\033[92mNOTICE: " + message + "\033[0m")
+        elif mtype == self.MWARNING:
+            print("\033[93mWARNING: " + message + "\033\[0m]")
+        elif mtype == self.MERROR:
+            print("\033[91mERROR: " + message + "\033[0m")
+        elif mtype == self.MDEBUG:
+            print("\033[95mDEBUG: " + message + "\033[0m")
 
     @staticmethod
     def test_matroska(path):
+        """Test if file is Matroska data."""
         with magic.Magic() as m:
             if "matroska" in m.id_filename(path).lower():
                 return True
-        print("File is not matroska.")
         return False
 
 if __name__ == '__main__':
